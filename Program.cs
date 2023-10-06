cc93c598b292468e4b8ee988ca2c4c78b6572029
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-10-06 16:35:13 +0200

    Lagt till en while-loop i Program.Meny samt lagt till fler metoder.

--- Program.cs
+++ Program.cs
@@ -9,37 +9,7 @@
 
 
         }
-        static void Meny()
-        {
-            Console.WriteLine("Välj vad du vill göra från menyn genom att ange korresponderande siffra:");
-            Console.WriteLine("1. Se dina konton och saldo");
-            Console.WriteLine("2. Överföringar mellan konton");
-            Console.WriteLine("3. Ta ut pengar");
-            Console.WriteLine("4. Logga ut");
-            int result = 0;
-            string choices = Console.ReadLine();
-            if (Int32.TryParse(choices, out result) || (result >= 1 && result < 5))
-            {
-                switch (result)
-                {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
 
-                }
-            }
-            else
-            {
-                Console.WriteLine("Du måste skriva in en siffra mellan 1-4.");
-            }
-
-
-        }
         public static void Loggin()
         {
             Console.WriteLine("Välkommen till Awesome Bank!");
@@ -52,7 +22,64 @@
             Console.WriteLine("Pinkod:");
             int pin = Convert.ToInt32(Console.ReadLine());
         }
-        public static void UserAcc()
+        public static void Welcome()
+        {
+
+        }
+        static void Meny()
+        {
+            Console.WriteLine("Välj vad du vill göra från menyn genom att ange korresponderande siffra:");
+            Console.WriteLine("1. Se dina konton och saldo");
+            Console.WriteLine("2. Överföringar mellan konton");
+            Console.WriteLine("3. Ta ut pengar");
+            Console.WriteLine("4. Logga ut");
+            int result = 0;
+            string choices = Console.ReadLine();
+            bool loggedin = true;
+            while (loggedin)
+            {
+                if (Int32.TryParse(choices, out result) || (result >= 1 && result < 5))
+                {
+                    switch (result)
+                    {
+                        case 1:
+                            Program.UserAccInfo();
+                            break;
+                        case 2:
+                            Program.Transfer();
+                            break;
+                        case 3:
+                            Program.Withdraw();
+                            break;
+                        case 4:
+                            loggedin = false;
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Du måste skriva in en siffra mellan 1-4.");
+                }
+
+            }
+
+
+
+        }
+        public static void StoreUsers()
+        {
+
+        }
+        public static void ValidateUserName()
+        {
+
+        }
+        public static void ValidatePin()
+        {
+
+        }
+        public static void UserAccInfo()
         {
 
         }
@@ -64,5 +91,6 @@
         {
 
         }
+
     }
 }
